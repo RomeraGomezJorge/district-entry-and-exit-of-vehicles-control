87adaba91459de655cdfdb7aaa00fc6556ea861d
User <romera.gomez.jorge@gmail.com>
2021-01-28 21:58:37 -0300

    Se repara la falla que se produce al presionar enter en un formulario y no se agreguen la reglas de validacion ( por ej:  validar que un valor sea unico) que estas se disparan al sacar el foco de un campo del formulario

--- public/assets/js/blog/form/click.submit.button.on.enter.key.press.in.keyboard.js
+++ public/assets/js/blog/form/click.submit.button.on.enter.key.press.in.keyboard.js
@@ -2,6 +2,7 @@ function disablingEnteKeyForForm() {
     $(document).keypress(
         function(event){
             if (event.which == '13') {
+                $('button[type="submit"]').focus();
                 event.preventDefault();
                 $('button[type="submit"]').click();
             }
